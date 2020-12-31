9ad5bda29c27df6fb1be4e0689333fbacd6a0324
서준혁 <taulmax@onpia.co.kr>
2020-12-31 14:02:59 +0900

    sayHi

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "Nico",
+  age = 23,
+  gender = "male";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
